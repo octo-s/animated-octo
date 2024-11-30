dbc39ae68dd93134e517e4ac8a4b8870e7d18757
tatyana.lebedeva <tslebedeva3103@gmail.com>
2024-11-30 19:30:13 +0100

    add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: '/animated-octo/',
